292be8367bc26df223c886a3c154a08a8a6fa324
gisotra <gisotra.henrique@gmail.com>
2026-01-12 11:38:29 -0300

    interrupted once again :(

--- scripts/weapons/bullet_shell.gd
+++ scripts/weapons/bullet_shell.gd
@@ -6,24 +6,24 @@ class_name BulletShell
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var duration: float
 @onready var shell_texture: Texture2D
-@onready var new_point_floor: Vector2
+@onready var new_point_floorY: float
+
 
 func _initialize(tempoDeDuracao: float, sprite: Texture2D):
 	duration = tempoDeDuracao
 	shell_texture = sprite
 	if duration and shell_texture:
 		print("capsula configurada!")
-	
-
 
 func _ready():
 	#anim_player.play("birth")	
 	spr_shell.texture = shell_texture
 	death_timer.wait_time = duration
 	death_timer.start()
-	new_point_floor.x = randf_range(get_parent().position.x - 50, get_parent().position.x + 50)
-	new_point_floor.y = randf_range(get_parent().position.y - 50, get_parent().position.y + 50)
+	new_point_floorY = randf_range(get_parent().position.y - 50, get_parent().position.y + 50)
 	
+	
+func _physics_process(delta: float):
 	
 	
 
