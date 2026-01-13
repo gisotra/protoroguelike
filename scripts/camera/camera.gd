fa3211ffcfe2794d2b0c59ded360a698cc568216
gisotra <gisotra.henrique@gmail.com>
2026-01-13 08:09:22 -0300

    guided camera

--- scripts/camera/camera.gd
+++ scripts/camera/camera.gd
@@ -2,6 +2,11 @@ extends Camera2D
 class_name GameCamera
 
 @export var player: Node2D
+@export var camera_states: states
+enum states {
+	AIM,
+	GUIDED
+}
 
 #Mouse Oriented Camera
 var desired_offset: Vector2
@@ -12,18 +17,26 @@ var max_off_set = 50
 var shake_fade: float = 10.0
 var shake_strength: float
 
+func _ready():
+	camera_states = states.AIM
+	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	desired_offset = (get_global_mouse_position() - position) * 0.5
-	desired_offset.x = clamp(desired_offset.x, min_off_set, max_off_set)
-	desired_offset.y = clamp(desired_offset.y, min_off_set / 2.0, max_off_set / 2.0)
+	match camera_states:
+		states.AIM:
+			desired_offset = (get_global_mouse_position() - position) * 0.5
+			desired_offset.x = clamp(desired_offset.x, min_off_set, max_off_set)
+			desired_offset.y = clamp(desired_offset.y, min_off_set / 2.0, max_off_set / 2.0)
 	
-	global_position = player.global_position + desired_offset
-	#handles shaking
-	if shake_strength > 0:
-		shake_strength = lerp(shake_strength, 0.0, shake_fade * delta)
-		offset = Vector2(randf_range(-shake_strength, shake_strength), randf_range(-shake_strength, shake_strength))
-		
+			global_position = player.global_position + desired_offset
+			#handles shaking
+			if shake_strength > 0:
+				shake_strength = lerp(shake_strength, 0.0, shake_fade * delta)
+				offset = Vector2(randf_range(-shake_strength, shake_strength), randf_range(-shake_strength, shake_strength))
+		#states.GUIDED:
+			#print("omg")
 func trigger_shake(max_shake: float):
-	shake_strength = max_shake
+	if camera_states == states.AIM:
+		shake_strength = max_shake
 	
