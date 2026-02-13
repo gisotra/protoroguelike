5d00b892dcda763a17950bcce623dc9d74f1ada8
gisotra <gisotra.henrique@gmail.com>
2026-02-13 14:30:34 -0300

    health heal over time

--- scenes/entities/static/train_dummy.gd
+++ scenes/entities/static/train_dummy.gd
@@ -1,8 +1,19 @@
 extends Enemy
+class_name Dummy
 
+@onready var time_to_heal: Timer = $time_to_heal
 @onready var hit_fx: GPUParticles2D = $hit_FX
 
+
+func _process(delta: float) -> void :
+	if health.current_health < health.maxHealth and time_to_heal.is_stopped():
+		print("current health: ", health.current_health)
+		print("max health: ", health.maxHealth)
+		health.heal(1.5)
+		
 
 func _on_enemy_hurtbox_collided(damage, pos, knockback_vec) -> void:
 	hit_fx.rotation = knockback_vec.angle()
 	hit_fx.emitting = true
+	time_to_heal.start(3)
+	
