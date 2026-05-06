e9f2ee1f5d1ed3623d156cef7e61cd1728c3cada
gisotra <gisotra.henrique@gmail.com>
2026-05-06 08:19:26 -0300

    main ui first implementations

---
+++ components/health/health_ui.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+class_name GameUI
+
+@export var player : Node2D 
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scenes/player/scripts/player.gd
+++ scenes/player/scripts/player.gd
@@ -2,24 +2,20 @@ extends CharacterBody2D
 class_name Player
 
 @export var SPEED = 150.0
+@export_range(1, 10) var HEALTH : int
+ 
 const coeficiente_de_aceleracao = .15
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var eyes: Sprite2D = $eyes
 var isWalking: bool = false
 @onready var dust: GPUParticles2D = $dust
 @onready var central_head_point: Marker2D = $central_head_point
 @onready var frame_count: int = 0
-@onready var eyes_update_rate = 10
 @onready var weapon_manager: Node2D = $WeaponManager
 
 func _physics_process(delta: float) -> void:
 	frame_count += 1
 	#handle the direction the player is facing
 	_handle_direction()
-	
-	"""if frame_count % eyes_update_rate:
-		eyes._update_eyes_pos()
-		frame_count = 0"""
 	
 	#handle movement
 	var direction
@@ -54,10 +50,8 @@ func _handle_direction():
 	distX = position.x - get_global_mouse_position().x
 	if (distX >= 0): #looking to the right
 		sprite.scale.x = -1
-		eyes.scale.x = -1
 		central_head_point.scale.x = -1
 	else: #looking to the left
 		sprite.scale.x = 1
-		eyes.scale.x = 1
 		central_head_point.scale.x = 1
 		
